b156864d516992eb73d3cff77ed0cd911314aa5d
xogk1128 <xogk1128@naver.com>
2023-08-07 19:08:14 +0900

    add Kakao Login

---
+++ app/src/main/java/com/example/sumnote/ui/kakaoLogin/KakaoFragment.kt
@@ -0,0 +1,116 @@
+package com.example.sumnote.ui.kakaoLogin
+
+import android.annotation.SuppressLint
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.asLiveData
+import com.example.sumnote.R
+import com.example.sumnote.ui.kakaoLogin.KakaoViewModel.Companion.TAG
+import com.kakao.sdk.user.UserApiClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class KakaoFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = KakaoFragment()
+    }
+
+    private lateinit var viewModel: KakaoViewModel
+    var name: String = ""
+    var email: String = ""
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_kakao, container, false)
+    }
+
+    @SuppressLint("MissingInflatedId")
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this, KakaoOauthViewModelFactory(requireActivity().application)).get(KakaoViewModel::class.java)
+        // TODO: Use the ViewModel
+
+        val btnKakaoLogin = view?.findViewById<Button>(R.id.btn_kakao_login)
+        val btnKakaoLogout = view?.findViewById<Button>(R.id.btn_kakao_logout)
+        val tvLoginStatus = view?.findViewById<TextView>(R.id.tv_login_status)
+
+        btnKakaoLogin?.setOnClickListener {
+            viewModel.kakaoLogin()
+        }
+
+        btnKakaoLogout?.setOnClickListener {
+            viewModel.kakaoLogout()
+        }
+
+        viewModel.isLoggedIn.asLiveData().observe(viewLifecycleOwner) { isLoggedIn ->
+            val loginStatusInfoTitle = if (isLoggedIn) "로그인 상태" else "로그아웃 상태"
+            tvLoginStatus?.text = loginStatusInfoTitle
+        }
+
+        val test = view?.findViewById<Button>(R.id.buttonInfo)
+        test?.setOnClickListener {
+            getInfo()
+        }
+
+    }
+
+    fun getInfo(){
+        // 사용자 정보 요청 (기본)
+        UserApiClient.instance.me { user, error ->
+            if (error != null) {
+                Log.e(TAG, "사용자 정보 요청 실패", error)
+            }
+            else if (user != null) {
+                Log.i(TAG, "사용자 정보 요청 성공" +
+                        "\n회원번호: ${user.id}" +
+                        "\n이메일: ${user.kakaoAccount?.email}" +
+                        "\n닉네임: ${user.kakaoAccount?.profile?.nickname}" )
+
+                name = user.kakaoAccount?.profile?.nickname.toString()
+                email = user.kakaoAccount?.email.toString()
+                val userInfo = User()
+                userInfo.name = user.kakaoAccount?.profile?.nickname.toString()
+                userInfo.email = user.kakaoAccount?.email.toString()
+                Log.d("BUTTON CLICKED", "id: " + userInfo.name + ", pw: " + userInfo.email)
+
+                Login(userInfo)
+            }
+        }
+    }
+
+    fun Login(user: User){
+
+        val call = RetrofitBuilder.api.getLoginResponse(user)
+        Log.d("Login", "정보를 받았습니다 ${call}")
+        call.enqueue(object : Callback<String> { // 비동기 방식 통신 메소드
+            override fun onResponse( // 통신에 성공한 경우
+                call: Call<String>,
+                response: Response<String>
+            ) {
+                if(response.isSuccessful()){ // 응답 잘 받은 경우
+                    Log.d("RESPONSE: ", response.body().toString())
+
+                }else{
+                    // 통신 성공 but 응답 실패
+                    Log.d("RESPONSE", "FAILURE")
+                }
+            }
+
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                // 통신에 실패한 경우
+                Log.d("CONNECTION FAILURE: ", t.localizedMessage)
+            }
+        })
+    }
+
+}
---
+++ app/src/main/java/com/example/sumnote/ui/kakaoLogin/RetrofitBuilder.kt
@@ -0,0 +1,16 @@
+package com.example.sumnote.ui.kakaoLogin
+
+import com.example.sumnote.api.ApiManager
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitBuilder {
+    var api: ApiManager
+    init{
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://192.168.0.21:8080/") // 요청 보내는 API 서버 url. /로 끝나야 함함
+            .addConverterFactory(GsonConverterFactory.create()) // Gson을 역직렬화
+            .build()
+        api = retrofit.create(ApiManager::class.java)
+    }
+}
