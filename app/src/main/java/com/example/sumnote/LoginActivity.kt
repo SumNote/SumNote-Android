580bf76ccdf9c338ee1419b92ad38bb19adbadd6
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-08-20 00:40:36 +0900

    Brand Color 지정, Splash Activity 작성, 로그인 엑티비티 작성

--- app/src/main/java/com/example/sumnote/LoginActivity.kt
+++ app/src/main/java/com/example/sumnote/LoginActivity.kt
@@ -1,9 +1,12 @@
 package com.example.sumnote
 
+import android.animation.ObjectAnimator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.WindowManager
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.sumnote.databinding.ActivityLoginBinding
 import com.example.sumnote.databinding.ActivityMainBinding
@@ -17,6 +20,13 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+//        // Optional: Add a listener to splash screen
+//        splashScreen.setOnExitAnimationListener { splashScreenProvider ->
+//            val fadeOut = ObjectAnimator.ofFloat(splashScreenProvider.view, View.ALPHA, 0f)
+//            fadeOut.duration = 500L
+//            fadeOut.start()
+//        }
+
         val intent = Intent(this, MainActivity::class.java)
         //로그인 버튼 클릭시 : 카카오 로그인 수행하고 MainActivity로 넘어가기
         binding.btnLogin.apply {
@@ -28,5 +38,7 @@ class LoginActivity : AppCompatActivity() {
             }
         }
 
+        // 상태바 숨기기
+        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
