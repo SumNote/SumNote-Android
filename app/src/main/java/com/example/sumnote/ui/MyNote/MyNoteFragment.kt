7edac25ebcf94a0a811ef5071315e923e942e025
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-09-12 18:41:42 +0900

    노트 저장 : 노트 리스트 예시 코드 작성

--- app/src/main/java/com/example/sumnote/ui/MyNote/MyNoteFragment.kt
+++ app/src/main/java/com/example/sumnote/ui/MyNote/MyNoteFragment.kt
@@ -69,7 +69,7 @@ class MyNoteFragment : Fragment(){
         //id : note_list_recycler_view
 
         //리사이클러뷰에 사용할 아이템 리스트(테스트용)
-        var noteList = ArrayList<NoteItem>()
+        //var noteList = ArrayList<NoteItem>()
         //data class NoteItem constructor(var id:Int, var title:String, var generatedDate:String)
 //        for(i in 0 until 10){
 //            noteList.add(NoteItem(i, "Note $i","2023.08.30 pm 16:53"))
